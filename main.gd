a7964b778ef0603ccac8ed0935a181186d9d5e4f
Emma-John <redrose.esj@gmail.com>
2023-10-14 10:15:55 -0600

    Game

---
+++ main.gd
@@ -0,0 +1,51 @@
+extends Node
+
+@export var rigid_body_2d: PackedScene
+var score
+# Called when the node enters the scene tree for the first time.
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
+
+
+func _on_mob_timer_timeout():
+	var mob = rigid_body_2d.instantiate()
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.progress_ratio = randf()
+	
+	var direction = mob_spawn_location.rotation + PI/2
+	
+	mob.position = mob_spawn_location.position
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
+	
+	$HUD.update_score(score)
+	
+
+
+func _on_score_timer_timeout():
+	score += 1
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
